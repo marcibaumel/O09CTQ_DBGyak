7bf949bc9d91dbeb755d1573ffbab3a55c6cea40
Marthon <marcibaumel@gmail.com>
2020-11-24 09:41:19 +0100

    gyak11

--- O09CTQ_11.24/O09CTQ_11.2.3.sql
+++ O09CTQ_11.24/O09CTQ_11.2.3.sql
@@ -1,6 +1,47 @@
 use gyak11;
 
+/*
 SELECT autó.Rendszám, tulajdonos.Név
 FROM tulajdonos
 INNER JOIN autó
 ON tulajdonos.tkód=autó.Tulaj;
+*/
+
+/*
+SELECT autó.*
+FROM tulajdonos
+INNER JOIN autó
+ON tulajdonos.tkód=autó.Tulaj
+WHERE tulajdonos.Város='Miskolc';
+*/
+
+/*
+SELECT autó.Típus, autó.Ár, Kategoria.Knév
+FROM autó
+*/
+
+/*
+SELECT tulajdonos.Név, autó.Rendszám, tulajdonos.TKód
+FROM tulajdonos
+JOIN autó
+ON tulajdonos.tkód=autó.Tulaj
+*/
+
+/*
+SELECT tulajdonos.Név, autó.Rendszám, tulajdonos.TKód
+FROM tulajdonos
+WHERE autó.rendszám!=null
+JOIN autó
+ON tulajdonos.tkód=autó.Tulaj;
+*/
+
+/*
+SELECT autó.Rendszám
+FROM autó
+JOIN tulajdonos
+ON tulajdonos.tkód=autó.Tulaj
+WHERE avg(autó.Ár)>autó.Ár;
+*/
+
+
+
